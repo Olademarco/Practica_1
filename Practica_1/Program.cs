7f1a8541bd7746cd7aa69ac1397138a836827823
arodr <arodr@LAPTOP-G5JTKK88>
2022-09-05 23:50:34 -0700

    Agregar archivos de proyecto.

---
+++ Practica_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marco Antonio Rodriguez Mariscal");
+            Console.WriteLine("Mexicali B.C");
+            Console.WriteLine("22 años");
+            Console.ReadKey();
+        }
+    }
+}
